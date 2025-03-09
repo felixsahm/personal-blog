1526c6b751ddca269b4dd4d1e9d54d8e11c49a99
Felix Sahm <felixsahm@34e1f7f5-459c-4cfc-b6d4-ddb6fd7c6b58.fritz.box>
2025-03-09 12:31:42 +0100

    Add template rendering and jinja code

--- app.py
+++ app.py
@@ -1,17 +1,33 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 app.config.from_object('config')
 
 
-@app.route('/')
+cookies_data = [ #list
+  {'name': 'Chocolate Chip', 'price': '$1.50'},
+  {'name': 'Oatmeal Raisin', 'price': '$1.00'},
+  {'name': 'Sugar', 'price': '$0.75'},
+  {'name': 'Peanut Butter', 'price': '$0.50'},
+  {'name': 'Oatmeal', 'price': '$0.25'},
+  {'name': 'Salted Caramel', 'price': '$1.00'},
+]
+
+@app.route('/cookies/<int:id>')
+def cookie(id):
+  return '<h1>' + cookies_data[id]['name'] + '</h1>' + '<p>' + cookies_data[id]['price'] + '</p>'
+
+@app.route('/index.html')
 def index():
-  return '<h1>Hello World!</h1>'
+  return render_template('index.html')
 
+@app.route('/posts.html')
+def posts():
+  return render_template('posts.html')
 
-@app.route('/about-me')
-def about_me():
-  return redirect(url_for('about'))
+@app.route('/about.html')
+def about():
+  return render_template('about.html')
 
 if __name__ == '__main__':
   app.run()
