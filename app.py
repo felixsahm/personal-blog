183f2d80f6bbce6eddb6fe313972a58fa87cc9b1
Felix Sahm <felixsahm@MacBook-Air-von-Felix.fritz.box>
2025-03-09 09:36:02 +0100

    Continue

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 app = Flask(__name__)
 
 app.config.from_object('config')
 
+
 @app.route('/')
 def index():
-  return 'Hello World!'
+  return '<h1>Hello World!</h1>'
+
+
+@app.route('/about-me')
+def about_me():
+  return redirect(url_for('about'))
 
 if __name__ == '__main__':
   app.run()
